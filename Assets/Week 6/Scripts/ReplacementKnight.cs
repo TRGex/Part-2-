8329e8988969dc417dbc596947766fc2f5e52468
LAPTOP-OF-ALEX\Alex <carrasal@sheridancollege.ca>
2024-02-14 09:43:04 -0500

    Task 1 complete: Player doesn't move to UI buttons

--- Assets/Week 6/Scripts/ReplacementKnight.cs
+++ Assets/Week 6/Scripts/ReplacementKnight.cs
@@ -1,23 +1,23 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Knight : MonoBehaviour
+public class ReplacementKnight : MonoBehaviour
 {
-    Rigidbody2D rb;
-    Animator animator;
     Vector2 destination;
     Vector2 movement;
-    public float speed = 3;
-    bool clickingOnSelf = false;
+    public float speed = 3f;
+    bool clickingOnSelf;
+    public float maxHealth = 10;
     public float health;
-    public float maxHealth = 5;
     bool isDead;
-    public HealthBar healthbar;
 
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -28,7 +28,7 @@ public class Knight : MonoBehaviour
     private void FixedUpdate()
     {
         if (isDead) return;
-        
+
         movement = destination - (Vector2)transform.position;
 
         if (movement.magnitude < 0.1)
@@ -38,33 +38,37 @@ public class Knight : MonoBehaviour
 
         rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
     }
-
-    void Update()
+    private void Update()
     {
         if (isDead) return;
 
-        if (Input.GetMouseButtonDown(0) && !clickingOnSelf)
+        if (Input.GetMouseButtonDown(0) && !clickingOnSelf && !EventSystem.current.IsPointerOverGameObject())
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
+
+        if (Input.GetMouseButtonDown(1) && !clickingOnSelf)
+        {
+            animator.SetTrigger("Attack");
+        }
+
         animator.SetFloat("Movement", movement.magnitude);
     }
     private void OnMouseDown()
     {
         if (isDead) return;
         clickingOnSelf = true;
-        gameObject.SendMessage("TakeDamage", 1);
+        SendMessage("TakeDamage", 2);
     }
-
     private void OnMouseUp()
     {
         clickingOnSelf = false;
     }
     public void TakeDamage(float damage)
     {
-        health = health -= damage;
+        health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
-        if (health <= 0)
+        if (health == 0)
         {
             isDead = true;
             animator.SetTrigger("Death");
