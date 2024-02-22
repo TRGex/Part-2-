9a0443d021913c6e9dddf53cc5c0ae62c910ef6a
LAPTOP-OF-ALEX\Alex <carrasal@sheridancollege.ca>
2024-02-21 22:17:57 -0500

    Task 1 complete: Added cannon that slides back and forth with the mouse

---
+++ Assets/Assignment/Scripts/Cannon Movement.cs
@@ -0,0 +1,38 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonMovement : MonoBehaviour
+{
+   
+    Vector2 cannonPosition;
+    Vector2 movement;
+    public float speed = 5f;
+    public GameObject cannonBall;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cannonPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(cannonBall, transform.position, transform.rotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        movement = cannonPosition - (Vector2)transform.position;
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+}
